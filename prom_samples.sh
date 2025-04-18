ac59c249b41b766560702b5f17f0bed33b1d27c6
Alejandro Kolton <47402803+droyktton@users.noreply.github.com>
2025-04-17 21:53:59 -0300

    Update prom_samples.sh

--- prom_samples.sh
+++ prom_samples.sh
@@ -5,7 +5,7 @@ echo "samples="$samples
 for((i=1;i<$samples;i++))
 do 
     echo $i; 
-    ./a.out 1024 1000000 $i; 
+    ./activeinterface 1024 1000000 $i; 
     cp inst_sofq.dat "inst_sofq_"$i"_.dat"; 
     cp cm.dat "cm_"$i"_.dat";
 done

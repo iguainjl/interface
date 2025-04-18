f77e425a3acbb561a0f242c652d88543544fd548
Alejandro Kolton <47402803+droyktton@users.noreply.github.com>
2025-04-18 20:00:02 -0300

    Update prom_samples.sh

--- prom_samples.sh
+++ prom_samples.sh
@@ -13,6 +13,6 @@ done
 paste inst_sofq_*_.dat | awk '{acum=0; for(i=0;i<NF;i++){acum+=$i}; if(NF>0) print acum*1.0/NF; else print;}' \
 > "sofq_"$samples"samples.dat"
 
-paste cm_*_.dat | awk '{acum=0; for(i=0;i<NF;i++){acum[i%6]+=$i}; if(NF>0){for(i=0;i<6;i++) print acum[i]*6/NF}; else print;}' \
-> "cm_"$samples"samples.dat"
+#paste cm_*_.dat | awk '{acum=0; for(i=0;i<NF;i++){acum[i%6]+=$i}; if(NF>0){for(i=0;i<6;i++) print acum[i]*6/NF}; else print;}' \
+#> "cm_"$samples"samples.dat"
 

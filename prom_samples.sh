7f6413ea2613696c767ed2c7e369f40cb3555044
Alejandro Kolton <47402803+droyktton@users.noreply.github.com>
2025-04-24 18:24:10 -0300

    Update prom_samples.sh

--- prom_samples.sh
+++ prom_samples.sh
@@ -1,14 +1,17 @@
 samples=$1
 L=$2
 Nrun=$3
+i0=$4
 
 echo "samples="$samples
 
 for((i=1;i<=$samples;i++))
 do 
-    echo $i; 
-    ./activeinterface $L $Nrun $i; 
-    cp inst_sofq.dat "inst_sofq_"$i"_.dat"; 
+    semilla=$(awk -v i=$i -v i0=$i0 'BEGIN{print i+i0}')
+    echo "semilla = "$semilla 
+
+    ./activeinterface $L $Nrun $semilla 
+    cp inst_sofq.dat "inst_sofq_"$i"_.dat" 
     cp cm.dat "cm_"$i"_.dat";
 done
 
